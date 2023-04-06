eb2f7c404ca1a68897a7dcbee084b6e1e5243cd7
Владимир Леонтьев <dllreg.exe@gmail.com>
2023-04-07 00:15:25 +0800

    edit setting

--- zproject/configured_settings.py
+++ zproject/configured_settings.py
@@ -6,19 +6,22 @@
 # (/etc/zulip/settings.py in production, or dev_settings.py or test_settings.py
 # in dev and test), we want to initialize them to sane defaults.
 from .default_settings import *  # noqa: F403 isort: skip
+from .dev_settings import *  # noqa: F403 isort: skip
 
 # Import variables like secrets from the prod_settings file
 # Import prod_settings after determining the deployment/machine type
-from .config import PRODUCTION
-
-if PRODUCTION:
-    from .prod_settings import *  # noqa: F403 isort: skip
-else:
+# ____
+# from .config import PRODUCTION
+#
+# if PRODUCTION:
+#     from .prod_settings import *  # noqa: F403 isort: skip
+# else:
+#___
     # For the Dev VM environment, we use the same settings as the
     # sample prod_settings.py file, with a few exceptions.
     # from .prod_settings_template import *  # noqa: F403 isort: skip
     # from .dev_settings import *  # noqa: F403 isort: skip
-    from .dev_settings import *  # noqa: F403 isort: skip
+    # from .dev_settings import *  # noqa: F403 isort: skip
 
 # Do not add any code after these wildcard imports!  Add it to
 # computed_settings instead.
